bc62f81f3ff74e44c7b05d70f0c285ad53fbb7ea
disar <disarra2002@gmail.com>
2021-03-21 10:42:15 +0300

    Доделал класс Автомата

--- app/src/main/java/com/example/vendingmachines/VendingMachine.java
+++ app/src/main/java/com/example/vendingmachines/VendingMachine.java
@@ -3,14 +3,16 @@ package com.example.vendingmachines;
 import com.example.vendingmachines.products.IProduct;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class VendingMachine {
     private String name;
     private ArrayList<IProduct> products;
     private Status status;
+    private Student client;
     private long clientNumber;
-    private long haveToPay;
-
+    private int haveToPay;
+    private int[] choose;
     private long cash;
 
     VendingMachine(String name) {
@@ -27,24 +29,40 @@ public class VendingMachine {
             products.addAll(newProducts);
     }
 
-    public void IsWorkingWithAClient() {
-        status = status.ISWORKINGWITHACLIENT;
+    public void startToWork(Student client) {
+        this.client = client;
+        this.clientNumber = client.getNumber();
+        status = Status.ISWORKINGWITHACLIENT;
     }
 
-    public void isPaying() {
+    public void workingWithAClient() {
+        choose = client.choose(products);
+        haveToPay = 0;
+        for (int i = 0; i < 3; i++) {
+            haveToPay += products.get(choose[i]).getCost();
+        }
         status = status.ISPAYING;
     }
 
-    public void doingNothing() {
-        status = status.DOINGNOTHING;
-    }
-
-    public void isDelivering() {
+    public void paying() {
+        client.pay(haveToPay);
+        cash += haveToPay;
+        haveToPay = 0;
         status = status.ISDELIVERING;
     }
 
-    public void PrepareToSell(int id) {
-        haveToPay += products.get(id).getCost();
+    public void delivering() {
+        for (int i = 0; i < 3; i++) {
+            products.remove(choose[i]);
+            try {
+                TimeUnit.SECONDS.sleep(1); // Секунда на удаление товара
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        haveToPay = 0;
+        clientNumber = 0;
+        status = status.DOINGNOTHING;
     }
 }
 
